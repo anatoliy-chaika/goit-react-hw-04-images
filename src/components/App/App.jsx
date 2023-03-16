73b1952a6d312239686dcbb88f6e4e3b4940fe03
anatoliy-chaika <110462151+anatoliy-chaika@users.noreply.github.com>
2023-03-16 16:34:59 +0200

    update App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,63 @@
-import { useState } from 'react';
-import { ToastContainer } from 'react-toastify';
+import { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { getImage } from 'components/Servises/GetImage';
+import { Loader } from 'components/Loader/Loader';
+import { Button } from '../Button/Button';
 import { GlobalStyles } from '../GlodalStyles/Global.styled';
 import ImageList from '../ImageGallery/ImageGallery';
 import Searchbar from '../Searchbar/Searchbar';
+import { ErrorMessage } from '../ImageGallery/ImageGallery.styled';
 import { AppDiv } from './App.styled';
 
 export function App() {
   const [searchImage, setSearchImage] = useState('');
+  const [images, setImages] = useState([]);
+  const [isLoad, setIsLoad] = useState(false);
+  const [error, setError] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalImg, setTotalImg] = useState(0);
 
-  const handleFormSubmit = searchImage => {
-    setSearchImage(searchImage);
+  const handleFormSubmit = newSearchImage => {
+    if (searchImage !== newSearchImage) {
+      setSearchImage(newSearchImage);
+      setPage(1);
+      setImages([]);
+      setTotalImg(0);
+    }
   };
+
+  const handleLoad = () => {
+    setPage(prev => prev + 1);
+  };
+
+  const isShow = page < Math.ceil(totalImg / 12);
+
+  useEffect(() => {
+    if (searchImage.trim() === '') {
+      return;
+    }
+
+    const fetchPictures = async () => {
+      setIsLoad(true);
+      try {
+        const { hits, totalHits } = await getImage(searchImage, page);
+
+        if (hits.length === 0) {
+          setIsLoad(false);
+          setImages([]);
+          setTotalImg(0);
+          return toast.error(`Nothing found for ${searchImage}!`);
+        }
+        setImages(prevImages => [...prevImages, ...hits]);
+        setIsLoad(false);
+        setTotalImg(totalHits);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchPictures();
+  }, [page, searchImage]);
 
   return (
     <AppDiv>
@@ -21,8 +68,10 @@ export function App() {
         }}
       />
       <Searchbar onSubmit={handleFormSubmit} />
-      <ImageList imageName={searchImage} />
-
+      <ImageList images={images} />
+      {isLoad && <Loader />}
+      {isShow && <Button onClick={handleLoad}></Button>}
+      {error !== '' && <ErrorMessage>{error}</ErrorMessage>}
       <GlobalStyles />
     </AppDiv>
   );
